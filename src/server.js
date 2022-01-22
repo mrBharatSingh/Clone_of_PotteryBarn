1ddd7620545b26cfaef6aaf371fad5b972f95fa2
mrBharatSingh <93440682+mrBharatSingh@users.noreply.github.com>
2022-01-23 01:03:12 +0530

    heroku port

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 const app = require("./index");
+const env = require("dotenv");
 
 const start = async () => {
-  app.listen(2233, () => {
+  app.listen(process.env.PORT || 2233, () => {
     console.log("listing on port no 2233");
   });
 };
